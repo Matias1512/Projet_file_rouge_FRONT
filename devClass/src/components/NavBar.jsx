0de414b2d680a7aefe97c61078b1582a0e1ff4a2
Matias1512 <matias.caturla@gmail.com>
2025-03-18 14:07:43 +0100

    add new icons on nav bar

--- devClass/src/components/NavBar.jsx
+++ devClass/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
-import { Box, VStack, Link, IconButton, Text, Flex, useColorModeValue, Button, useColorMode } from "@chakra-ui/react";
-import { FaHome, FaVolumeUp, FaDumbbell, FaTrophy, FaGift, FaShoppingBag, FaUser, FaPlus, FaSun, FaMoon } from "react-icons/fa";
+import { Box, VStack, Link, IconButton, Text, Flex, useColorModeValue, Button, useColorMode, Icon } from "@chakra-ui/react";
+import { FaHome, FaClock, FaDumbbell, FaTrophy, FaGift, FaShoppingBag, FaUser, FaPlus, FaSun, FaMoon } from "react-icons/fa";
 
 const NavItem = ({ href, icon, label, active, color }) => (
   <Link
@@ -13,8 +13,8 @@ const NavItem = ({ href, icon, label, active, color }) => (
     fontWeight="bold"
     fontSize="sm"
     transition="background-color 0.2s, color 0.2s"
-    bg={active ? "#e5f7d4" : "transparent"}
-    color={active ? "#58cc02" : color}
+    bg={active ? "blue.400" : "transparent"}
+    color={active ? "light" : color}
     _hover={{ bg: "gray.100", color: "gray.700" }}
   >
     <IconButton
@@ -38,15 +38,11 @@ const NavBar = () => {
     <Box as="nav" w="250px" minH="100vh" p={4} boxShadow="md" bg={colorMode} color={textColor}>
       <VStack spacing={2} align="stretch">
         <NavItem href="#" icon={<FaHome size={20} />} label="MON COURS" active color={textColor} />
-        <NavItem href="#" icon={<FaVolumeUp size={20} />} label="SONS" color={textColor} />
-        <NavItem href="#" icon={<FaDumbbell size={20} />} label="ENTRAÎNEMENT" color={textColor} />
-        <NavItem href="#" icon={<FaTrophy size={20} />} label="LIGUES" color={textColor} />
-        <NavItem href="#" icon={<FaGift size={20} />} label="QUÊTES" color={textColor} />
-        <NavItem href="#" icon={<FaShoppingBag size={20} />} label="BOUTIQUE" color={textColor} />
+        <NavItem href="#" icon={<FaClock size={20} />} label="DEFIS" color={textColor} />
         <NavItem href="#" icon={<FaUser size={20} />} label="PROFIL" color={textColor} />
-        <NavItem href="#" icon={<FaPlus size={20} />} label="PLUS" color={textColor} />
-        <Button onClick={toggleColorMode}>
-            Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+        <NavItem href="#" icon={<FaTrophy size={20} />} label="BADGE" color={textColor} />
+        <Button onClick={toggleColorMode} mt={4} leftIcon={<Icon as={colorMode === "light" ? FaMoon : FaSun} />}>
+        {colorMode === "light" ? "Mode Sombre" : "Mode Lumière"}
         </Button>
       </VStack>
     </Box>
